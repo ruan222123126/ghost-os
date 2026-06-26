da6a76796081c220a00032a804e332bc39db0547
ruan <1476600799@qq.com>
2026-06-26 23:06:13 +0800

    Disable unused Markstream optional peers

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,25 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   openAnalyzer: false,
 });
 
+const disabledMarkstreamOptionalPeers = [
+  '@antv/infographic',
+  '@terrastruct/d2',
+  'mermaid',
+  'stream-markdown',
+  'stream-monaco',
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   distDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      ...Object.fromEntries(disabledMarkstreamOptionalPeers.map((name) => [name, false])),
+    };
+    return config;
+  },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
